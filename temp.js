015d117d1f34fc0c3d1f15fc6a9ba94645d662e1
AxMari <AlexisMarien@gmail.com>
2016-04-18 13:31:23 -0400

    final

---
+++ temp.js
@@ -0,0 +1,20 @@
+var newTemp = "";
+
+function tempConvert(k,t) {
+	if(k === "C") {
+		newTemp = (t * 9/5) + 32;
+		console.log(newTemp + "°F");
+	}
+	else if(k === "F") {
+		newTemp = (t - 32) * (5/9);
+		console.log(newTemp + "°C");
+	}
+    else {
+        console.log("Err: Please enter either °C or °F");
+    }
+
+}
+
+tempConvert("C",0);
+tempConvert("F",212);
+tempConvert("K",45);
