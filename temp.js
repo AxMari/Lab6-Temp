c66976f8f6937d66bae7b3787a8b21bd432158ab
AxMari <AlexisMarien@gmail.com>
2016-04-18 14:47:22 -0400

    added prompt and alert

--- temp.js
+++ temp.js
@@ -1,19 +1,21 @@
+var temp = parseInt(prompt("Temperature", "Numerical Only"));
+var res = prompt("C or F?");
+var type = res.toLocaleUpperCase();
+
 var newTemp = "";
 
 function tempConvert(k,t) {
 	if(k === "C") {
 		newTemp = (t * 9/5) + 32;
-		console.log(newTemp + "°F");
+		alert(newTemp + "°F");
 	}
 	else if(k === "F") {
 		newTemp = (t - 32) * (5/9);
-		console.log(newTemp + "°C");
+		alert(newTemp + "°C");
 	}
 	else {
-        console.log("Err: Please enter either °C or °F");
+        alert("Err: Please enter either °C or °F");
 	}
 }
 
-tempConvert("C",0);
-tempConvert("F",212);
-tempConvert("K",45);
+tempConvert(type,temp);
